fca5091259e3e4d4fc19ff1a90f5c97f346be7c0
tagkyauita <yuta.jan.takagi@gmail.com>
2021-09-26 18:02:13 +0900

    edit: function boot (AppServiceProvider.php), function up (create_comments_table)

--- database/migrations/2021_09_26_173649_create_comments_table.php
+++ database/migrations/2021_09_26_173649_create_comments_table.php
@@ -14,6 +14,7 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
+            $table->string('comment', 40);
             $table->bigIncrements('id');
             $table->timestamps();
         });
