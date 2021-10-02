aebd7d17b1287a0b21dad2a0d281268803942a2d
haruna-ishii <harunyyyn.d16@gmail.com>
2021-10-02 16:07:44 +0900

    add:userstable}CO[VƃV[_[

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
         \Schema::defaultStringLength(191);
     }
 }
