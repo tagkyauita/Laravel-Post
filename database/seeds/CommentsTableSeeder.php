7aba9e7e11ba715abf6bd360dec98e7259f8ef8b
tagkyauita <yuta.jan.takagi@gmail.com>
2021-10-03 20:37:57 +0900

    exec:db seed

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -11,6 +11,17 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        for ($i = 1; $i <= 100; $i++) {
+            
+            $rand40 = str_random(40);
+
+            $params = [
+                'comment' => $rand40,
+                'post_id' => $i,
+            ];
+
+            DB::table('comments')->insert($params);
+
+        }
     }
 }
