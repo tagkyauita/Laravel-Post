17bca87bd290dbd282b81b193ef95120b960fc9b
tagkyauita <yuta.jan.takagi@gmail.com>
2021-10-03 20:23:24 +0900

    migrated:CommentTable, make:CommentsTableSeeder.php

---
+++ database/migrations/2021_10_03_090609_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->string('comment', 40);
+            $table->integer('post_id');
+            //$table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
