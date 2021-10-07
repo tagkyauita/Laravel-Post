406b9f838c21d57737ff8c20b59a4b09fb032346
haruna-ishii <harunyyyn.d16@gmail.com>
2021-10-07 21:37:46 +0900

    add.新規ユーザー登録の実装

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,47 +1,34 @@
 @extends('layouts.app')
+
 @section('content')
 
 <div class="container mt-5">
-    <div class="card">
-     <div class="front-wrap col-xs-6 col-lg-4">
-      <div class="form-group text-center">
-       <h2 class="logo-img mx-auto mt-5">新規登録</h2>
-       @include('error_card_list')
-      </div>
-      
-      {!! Form::open(['route' => 'signup.post']) !!}
-           <div class="form-group">
-               {!! Form::email('email', old('email'), ['class' => 'form-control','placeholder' => 'メールアドレス']) !!}
-           </div>
-
-           <div class="form-group">
-               {!! Form::text('name', old('name'), ['class' => 'form-control','placeholder' => 'ユーザー名'])　!!}
-           </div>
-
-           <div class="form-group">
-               {!! Form::password('password',['class' => 'form-control','placeholder' => 'パスワード'])　!!}
-           </div>
-
-           <div class="form-group">
-               {!! Form::password('password_confirmation', ['class' => 'form-control','placeholder' => 'パスワード再確認']) !!}
-           </div>
-
-           <div class="actions text-center">
-               {!! Form::submit('新規登録',['class' => 'btn btn-info w-auto']) !!}
-           </div>
-      {!! Form::close() !!}
-      <br>
-     </div>
-      　<p class="devise-link text-center">アカウントをすでにお持ちの場合⇨{!! link_to_route('login', 'ログインする') !!}</p>
+    <div class="card col-sm-8 m-auto">
+        <div class="form-wrap col-xs-8 col-lg-6 m-auto">
+            <div class="form-group text-center mb-0">
+            　　<h2 class="logo-img mx-auto font-weight-bold mb-5 mt-5">新規登録</h2>
+            </div>
+      　　{!! Form::open(['route' => 'signup.post']) !!}
+           　<div class="form-group">
+               　{!! Form::email('email', old('email'), ['class' => 'form-control','placeholder' => 'メールアドレス']) !!}
+           　</div>
+           　<div class="form-group">
+               　{!! Form::text('name', old('name'), ['class' => 'form-control','placeholder' => 'ユーザー名']) !!}
+           　</div>
+           　<div class="form-group">
+               　{!! Form::password('password', ['class' => 'form-control','placeholder' => 'パスワード']) !!}
+           　</div>
+           　<div class="form-group">
+               　{!! Form::password('password_confirmation', ['class' => 'form-control','placeholder' => 'パスワード再確認']) !!}
+           　</div>
+           　<div class="actions text-center">
+               　{!! Form::submit('新規登録',['class' => 'btn btn-info w-auto']) !!}
+           　</div>
+      　  {!! Form::close() !!}
+      　<br>
+        </div>
+      　{{-- <p class="devise-link text-center">アカウントをすでにお持ちの場合⇨{!! link_to_route('login', 'ログインする') !!}</p> --}}
     </div>  
 </div>
-@endsectiion
 
-
-
-
-
-
-
-
-
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup');
