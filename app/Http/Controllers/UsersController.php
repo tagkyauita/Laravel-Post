f11f2c40b41f7367988c79d6b64161abf653ae6f
tagkyauita <yuta.jan.takagi@gmail.com>
2021-10-09 01:30:21 +0900

    edit: UsersController -> show method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('users.show')->with('user', $user);
+    }
 }
