7c9a6fdb59fd7ae195cdaadb3e8b142acf32bd2d
tagkyauita <yuta.jan.takagi@gmail.com>
2021-10-11 13:47:14 +0900

    edit: users.blade.php => @if (->id == Auth::user->id())

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -8,17 +8,19 @@
             <div class="text-center">
                 <i class="fas fa-user-circle fa-3x"></i>
                 <h3>
-                　{{$user->name}}
+                　{{ $user->name }}
 　　　　         </h3>
                 <div class="col-12">
                     <p>
-                    {{$user->email}}
+                    {{ $user->email }}
                     </p>
                 </div>
 
                 <div class="col-12 mt-3">
-                    <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" href="#"><i class="fas fa-user-edit"></i>プロフィール編集
-                    </a>
+                    @if ($user->id == Auth::user->id())
+                        <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" href="#"><i class="fas fa-user-edit"></i>プロフィール編集
+                        </a>
+                    @endif
                     <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" rel="nofollow" data-method="POST" href="#"><i class="fas fa-cog"></i>ログアウト
                     </a>
                     <form id="logout-form" action="" method="POST" style="display: none;">{{ csrf_field() }}
